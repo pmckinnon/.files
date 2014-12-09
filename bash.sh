bd2b7dd1c91697bfaf6d5919929f77b13ffb6c5e
Patrick McKinnon <pmckinnon@lifesize.com>
2014-12-09 10:32:23 -0600

    - Stuff

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
-export PATH=~/bin:/usr/local/bin:~/packages/depot_tools:./node_modules/.bin:$PATH
+export PATH=~/bin:/usr/local/bin:~/packages/depot_tools:$PATH
 
 if [ -d ~/.bash_completion.d ]; then
   source ~/.bash_completion.d/git-prompt.sh
@@ -13,7 +13,7 @@ export PS1='\u@\[\033[1;34m\]\h\[\033[0m\]: \w$(__git_ps1 "(%s)")\$ '
 export CLICOLOR=1
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 
-alias grep="grep -I --exclude 'tags' --exclude 'tmp' --exclude-dir '.git' --exclude-dir '.svn'"
+alias grep="grep -I --exclude 'templates.js' --exclude 'app.js' --exclude 'tags' --exclude 'tmp' --exclude-dir '.git' --exclude-dir '.svn'"
 
 set -o vi
 set editing-mode vi
@@ -40,5 +40,12 @@ alias gen_passwd="openssl rand -base64 32"
 alias cpp_find_files="find . -name \"*.cc\" -o -name \"*.h\" -o -name \"*.cpp\""
 alias cpp_find="cpp_find_files | grep"
 alias cpp_grep="cpp_find_files | xargs grep"
+
+alias webrtc_find_files="find third_party/webrtc/ -name \"*.cc\" -o -name \"*.h\" -o -name \"*.cpp\""
+alias webrtc_find="webrtc_find_files | grep"
+alias webrtc_grep="webrtc_find_files | xargs grep"
+
+alias buildCSS='export VERSION=4.1.15.1.1 && cp ../../CSS/PSE.MS/build/psems.debug ../../CSS/PSE.MS/build/psems . && ant release'
+alias nat_discovery='java -cp ~/bin/NATCrackerDiscovery-0.1-SNAPSHOT-jar-with-dependencies.jar com.peerialism.natcracker.discovery.Main'
 
 # TODO: Write script to convert from 'https://github.com/pmckinnon/.files' style origin to 'github.com:pmckinnon/.files.git'
